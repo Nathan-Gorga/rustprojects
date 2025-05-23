3312b8bf20fb3be75aa083d1316b834ccd55a2dd
gorga.nathan05@gmail.com <gorga.nathan05@gmail.com>
2025-05-23 23:34:17 +0200

    task.rs explanations for rust features

--- src/project3/task.rs
+++ src/project3/task.rs
@@ -1,9 +1,18 @@
+/*WHAT IS #[derive(Debug)]
+
+derive asks the compiler to create a Debug trait
+
+a debug trait helps for debugging using println!("{:?}", var);*/
 #[derive(Debug)]
 pub struct Task{
     pub description: String,
     pub completed: bool,
 }
 
+
+/*WHAT IS IMPL
+
+used to associate functions (methods) tout a struct, enum or trait*/
 impl Task{
     pub fn new(description: String) -> Self {
         Task {
