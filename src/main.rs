e3fe696a4b74ee5a008269a03fc1bd25e12283d1
gorga.nathan05@gmail.com <gorga.nathan05@gmail.com>
2025-05-23 22:55:27 +0200

    create project 3

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 // mod project1;
 // use project1::*;
 
-mod project2;
-use project2::*;
+// mod project2;
+// use project2::*;
 
+mod project3;
+use project3::*;
 
 fn main(){
     //project1();
-    project2();
+    // project2();
+    project3();
 }
